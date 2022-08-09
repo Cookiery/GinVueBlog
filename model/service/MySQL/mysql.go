590e2449fde15afc6c5c4b417663fbe951bdcc92
Cookiery <2061803022@qq.com>
2022-08-09 11:31:38 +0800

    11

--- model/service/MySQL/mysql.go
+++ model/service/MySQL/mysql.go
@@ -3,7 +3,6 @@ package MySQL
 import (
 	"database/sql"
 	"fmt"
-	"strings"
 
 	"github.com/BurntSushi/toml"
 	_ "github.com/go-sql-driver/mysql"
@@ -22,22 +21,15 @@ func InitDB() *sql.DB {
 	var (
 		dbConf DBConf
 		err    error
-		dsn    strings.Builder // Data Source Name
+		dsn    string // Data Source Name
 	)
 	if _, err = toml.DecodeFile("./conf/MySQL.toml", &dbConf); err != nil {
 		// 这里打个日志
 		fmt.Println(err)
 	}
-	dsn.WriteString(dbConf.User)
-	dsn.WriteString(":")
-	dsn.WriteString(dbConf.PassWord)
-	dsn.WriteString("@tcp(")
-	dsn.WriteString(dbConf.Host)
-	dsn.WriteString(":")
-	dsn.WriteString(dbConf.Port)
-	dsn.WriteString(")/user")
+	dsn = dbConf.User + ":" + dbConf.PassWord + "@tcp(" + dbConf.Host + ":" + dbConf.Port + ")/user"
 	// dsn := "root:123456@tcp(127.0.0.1:3306)/user"
-	db, err := sql.Open("mysql", dsn.String())
+	db, err := sql.Open("mysql", dsn)
 	if err != nil {
 		fmt.Println(err)
 	}
